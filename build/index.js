8593644137cf1d719d045fb218fd4b7bc6ae87d2
THANG <thang@THANGs-MacBook-Air.local>
2022-06-28 10:54:53 +0700

    initilize project

---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const Thang = {
+    name: 'Thang Le Duc',
+    age: 23
+};
+console.log(Thang);
+console.log('Hello');
---
+++ index.ts
@@ -0,0 +1,12 @@
+interface Person {
+    name: string,
+    age: number
+}
+
+const Thang: Person = {
+    name: 'Thang Le Duc',
+    age: 23
+}
+
+console.log(Thang)
+console.log('Hello')
