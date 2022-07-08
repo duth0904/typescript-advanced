ef0a7f4704422e6a1bce867d11accf1a51fac41b
THANG <thang@THANGs-MacBook-Air.local>
2022-07-08 19:21:01 +0700

    why should use generic type

---
+++ build/src/use-generic.js
@@ -0,0 +1,28 @@
+"use strict";
+const getDisplayName = (item) => {
+    if ('name' in item) {
+        return {
+            displayName: item.name
+        };
+    }
+    return {
+        displayName: item.firstName + ' ' + item.lastName
+    };
+};
+// type AnimalOrHuman<T extends Human | Animal> = T extends Human ? {humanName: string} : {animalName: string}
+// const getDisplayName = <TItem extends Animal | Human> (item : TItem) : AnimalOrHuman <TItem> => {
+//     if ('name' in item) {
+//         return {
+//             animalName: item.name
+//         } as AnimalOrHuman <TItem>
+//     }
+//     return {
+//         humanName: item.firstName + ' ' + item.lastName
+//     } as  AnimalOrHuman <TItem>
+// }
+const result = getDisplayName({ name: 'Patch' });
+const result2 = getDisplayName({
+    firstName: 'Matt',
+    lastName: 'Robert'
+});
+console.log({ result2 });
